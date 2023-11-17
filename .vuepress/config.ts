b60757351268e65c85a9154f10d7367ad5926e0b
sanjana-moudgalya <112443767+sanjana-moudgalya@users.noreply.github.com>
2023-11-17 16:58:00 -0500

    Update config.ts

--- .vuepress/config.ts
+++ .vuepress/config.ts
@@ -6,7 +6,7 @@ export default defineUserConfig({
   lang: "en-US",
   title: "Evaluation of Machine Translations with Large Language Models",
   description: "A Blog for Machine Learning, Natural Language Processing, and Data Mining",
-  base: "/mnlp_blog/",
+  base: "/blog/",
   theme,
   plugins: [
     searchPlugin({
